d8ece762a57dccb9f003c0d082bc6ebeb6fdf309
nwind21 <poit21@gmail.com>
2014-09-15 07:55:15 -0700

    Reorganization of the test files.

---
+++ test/mock/ILightingMock.h
@@ -0,0 +1,32 @@
+﻿#include "ILighting.h"
+
+class ILightingMock : public ILighting
+{
+public:
+    virtual ~ILightingMock( ) {}
+
+    MOCK_METHOD0( initialize, void() );
+
+    MOCK_METHOD0( on, void() );
+    MOCK_METHOD1( on, void( ILighting::ARM arm ) );
+    MOCK_METHOD2( on, void( ILighting::ARM arm,
+                            uint8_t index ) );
+    MOCK_METHOD0( off, void( ) );
+    MOCK_METHOD1( off, void( ILighting::ARM arm ) );
+    MOCK_METHOD2( off, void( ILighting::ARM arm,
+                             uint8_t index ) );
+    MOCK_METHOD0( cnt, uint8_t( ) );
+    MOCK_METHOD1( length, uint8_t( ILighting::ARM arm ) );
+    MOCK_METHOD3( setColor, void( ILighting::ARM arm,
+                                  uint8_t index,
+                                  uint32_t color ) );
+    MOCK_METHOD2( setColor, void( ILighting::ARM arm,
+                                  uint32_t color ) );
+    MOCK_METHOD3( setColorAndDisplay, void ( ILighting::ARM arm,
+                                             uint8_t index,
+                                             uint32_t color ) );
+    MOCK_METHOD2( setColorAndDisplay, void( ILighting::ARM arm,
+                                            uint32_t color ) );
+    MOCK_METHOD1( strobe, void( uint8_t times ) );
+
+};
---
+++ test/mock/ISystemMock.h
@@ -0,0 +1,10 @@
+﻿#include "ISystem.h"
+
+class ISystemMock : public ISystem
+{
+public:
+    virtual ~ISystemMock() {}
+
+    MOCK_METHOD1( sleep, void( uint32_t val ) );
+
+};
